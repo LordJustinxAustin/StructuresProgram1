d346d65889c24bb1ff0bff7e9bac88e7463e98e5
LordJustinxAustin <justin.austin@bison.howard.edu>
2017-01-28 00:57:27 -0500

    StructuresProgram1/

---
+++ StructuresProgram1/StructuresProgram1/reverse-general.cpp
@@ -0,0 +1,9 @@
+//
+//  reverse-general.cpp
+//  StructuresProgram1
+//
+//  Created by Justin Austin on 1/28/17.
+//  Copyright © 2017 Justin Austin. All rights reserved.
+//
+
+#include "reverse-general.hpp"
---
+++ StructuresProgram1/StructuresProgram1/reverse-general.hpp
@@ -0,0 +1,27 @@
+//
+//  reverse-general.hpp
+//  StructuresProgram1
+//
+//  Created by Justin Austin on 1/28/17.
+//  Copyright © 2017 Justin Austin. All rights reserved.
+//
+
+#ifndef reverse_general_hpp
+#define reverse_general_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+//Header file
+using namespace std;
+
+class reverse-general
+{
+    private:
+    
+    public:
+}
+
+#endif /* reverse_general_hpp */
